2238b3bcd46518df475a6c6a0a15b279ae99971d
AshersLab <Asher.parry@icloud.com>
2025-10-04 02:41:25 +1000

    Create zonemaster_proto.cpp

---
+++ components/zonemaster_proto/zonemaster_proto.cpp
@@ -0,0 +1,6 @@
+#include "zonemaster_proto.h"
+namespace esphome {
+namespace zonemaster_proto {
+// All logic is in the header (header-only style for simplicity).
+}  // namespace zonemaster_proto
+}  // namespace esphome
