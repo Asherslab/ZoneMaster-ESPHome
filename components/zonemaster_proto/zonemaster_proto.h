87bf2c0a81ed851ec5c47ec974394d4795f56ea9
AshersLab <Asher.parry@icloud.com>
2025-10-04 08:40:20 +1000

    Update zonemaster_proto.h

--- components/zonemaster_proto/zonemaster_proto.h
+++ components/zonemaster_proto/zonemaster_proto.h
@@ -97,7 +97,9 @@ class ZonemasterProto : public Component, public uart::UARTDevice {
       // consume up to end
       buf_.erase(buf_.begin(), it_end + 1);
 
+      ESP_LOGI(TAG, "Size: %X", fr.size());
       if (fr.size() < 9) continue;
+      ESP_LOGI(TAG, "RX: %02X %02X %02X %02X %02X %02X %02X %02X %02X", fr[0], fr[1], fr[2], fr[3], fr[4], fr[5], fr[6], fr[7], fr[8]);
 
       // Identify response: AA 30 00 ID 81 01 DATA CRC 55
       const bool looks_response = (fr[0] == 0xAA && fr[1] == 0x30 && fr[2] == 0x00 && fr.back() == 0x55);
@@ -118,7 +120,7 @@ class ZonemasterProto : public Component, public uart::UARTDevice {
         ESP_LOGW(TAG, "CRC mismatch (exp=%02X got=%02X), dropping", crc_calc, crc_rx);
         continue;
       }
-
+     
       if (looks_response) {
         if (fr.size() < 9) continue;
         const uint8_t id   = fr[3];
@@ -128,7 +130,6 @@ class ZonemasterProto : public Component, public uart::UARTDevice {
           ESP_LOGV(TAG, "Response for other ID 0x%02X (=0x%02X), ignoring", id, this->device_id_);
           continue;
         }
-        ESP_LOGI(TAG, "RX: %02X %02X %02X %02X %02X %02X %02X %02X %02X", fr[0], fr[1], fr[2], fr[3], fr[4], fr[5], fr[6], fr[7], fr[8]);
         this->publish_buttons_(data);
       } else {
         // Valid request seen on bus (optional to log/debug)
